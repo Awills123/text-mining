d3975955963b2cc3a165c57c23272beabf4bf0cc
Awills123 <Awills1@babson.edu>
2021-10-28 14:45:58 -0400

    work

--- data_analysis.py
+++ data_analysis.py
@@ -4,23 +4,37 @@ url = 'https://www.gutenberg.org/files/59603/59603-0.txt'
 response = urllib.request.urlopen(url)
 data = response.read()  # a `bytes` object
 text = data.decode('utf-8')
-print(text) # for testing
+# print(text) # for testing
 
 url = 'https://www.gutenberg.org/files/59603/59603-0.txt'
 response = urllib.request.urlopen(url)
 data = response.read()  # a `bytes` object
-text = data.decode('utf-8')
-print(text) # for testing
+text1 = data.decode('utf-8')
+# print(text1) # for testing
+
+def word_frequency():
+    d = dict()
+    for line in text: 
+        line = line.strip()
+        words = line.split(" ")
+        for word in words: 
+            if word in d: 
+                d[word] = d[word]+ 1 
+            else: 
+                d[word] = 1 
+    for key in list(d.keys()):
+        return key, ":", d[key]
+
+print(word_frequency(text1))
+
+import pickle
+
+# Save data to a file (will be part of your data fetching script)
+
+with open('dickens_texts.pickle','w') as f:
+    pickle.dump(data.txt,f)
 
 
-# import pickle
-
-# # Save data to a file (will be part of your data fetching script)
-
-# with open('dickens_texts.pickle','w') as f:
-#     pickle.dump(,f)
-
-
-# # Load data from a file (will be part of your data processing script)
-# with open('dickens_texts.pickle','r') as input_file:
-#     reloaded_copy_of_texts = pickle.load(input_file)
+# Load data from a file (will be part of your data processing script)
+with open('dickens_texts.pickle','r') as input_file:
+    reloaded_copy_of_texts = pickle.load(input_file)
---
+++ reflection.markdown
@@ -0,0 +1,5 @@
+# Reflection 
+
+## Project Overview
+
+For my project I decided to use Project Gutenburg and analyze two pieces of writing done by Ernest Hemingway. The goal of looking at Ernest Hemingway specifically was to see the similarities in his writing. To do this I used 
