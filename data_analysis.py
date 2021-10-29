5e7bb1926ae57e629af948a0b78dfa0bd09e9919
Awills123 <Awills1@babson.edu>
2021-10-29 18:11:26 -0400

    work on assigment 2

--- data_analysis.py
+++ data_analysis.py
@@ -1,21 +1,54 @@
+from typing import Text
 import urllib.request
+import string
+import sys 
+from unicodedata import category 
 
-url = 'https://www.gutenberg.org/files/59603/59603-0.txt'
-response = urllib.request.urlopen(url)
-data = response.read()  # a `bytes` object
-text = data.decode('utf-8')
-# print(text) # for testing
+def make_request(url):
+    ''' Generic function that makes a request to the given url and returns the response '''
+    response = urllib.request.urlopen(url)
+    data = response.read()  # a `bytes` object
+    text = data.decode('utf-8')
+    return text
 
-url = 'https://www.gutenberg.org/files/59603/59603-0.txt'
-response = urllib.request.urlopen(url)
-data = response.read()  # a `bytes` object
-text1 = data.decode('utf-8')
-# print(text1) # for testing
+def remove_header(text):
+    ''' Finds indexes of starting and ending strings and returns a sliced text removing the indexes found'''
+    start_indicator = "*** START OF THIS PROJECT GUTENBERG EBOOK MARK TWAIN'S SPEECHES ***"
+    end_indicator = "End of the Project Gutenberg EBook of Mark Twain's Speeches by Mark"
+    # find the position of the starting string
+    start_index = text.find(start_indicator)
+    # find the postiion of the ending string
+    end_index = text.find(end_indicator)
+    # add on the length of the string to starting index so it chops the whole string rather than just the first character
+    slice_start_index = start_index + len(start_indicator)
+    # return sliced string
+    return text[slice_start_index:end_index]
 
-def word_frequency():
+
+def clean_data(text):
+    ''' removes all puncuation from strings and returns a new list of cleaned strings'''
+    words_in_speech = list(text.split())
+    string_list = []
+    # borrowed from professor's code
+    strippables = ''.join([chr(i) for i in range(sys.maxunicode) if category(chr(i)).startswith("P")])
+    for word in words_in_speech:
+        # make all words lowercase
+        word = word.lower()
+        # remove strippable characters from the end and beginning of the string
+        word = word.strip(strippables)
+        # remove strippable characters from the middle of the string
+        word = word.replace(strippables, '')
+        table = str.maketrans(dict.fromkeys(string.punctuation))
+        new_s = word.translate(table)       
+        string_list.append(new_s)
+    # print(string_list)
+    return string_list
+
+
+
+def word_frequency(string_list):
     d = dict()
-    for line in text: 
-        line = line.strip()
+    for line in string_list: 
         words = line.split(" ")
         for word in words: 
             if word in d: 
@@ -23,18 +56,56 @@ def word_frequency():
             else: 
                 d[word] = 1 
     for key in list(d.keys()):
-        return key, ":", d[key]
+        print(key, ":", d[key])
+    return d
 
-print(word_frequency(text1))
+
+
+def most_common(cleaned_data):
+    """Makes a list of word-freq pairs(tuples) in descending order of frequency.
+    hist: map from word to frequency
+    excluding_stopwords: a boolean value. If it is True, do not include any stopwords in the list.
+    returns: list of (frequency, word) pairs
+    """
+    t = []
+    for word, freq in cleaned_data.items():
+        t.append((freq,word))
+    t.sort()
+    t.reverse()
+    return t
+
+        
+
+
+def main():
+    text = make_request('https://www.gutenberg.org/files/3188/3188-0.txt')
+    removed_header_footer = remove_header(text)
+    cleaned_data = clean_data(removed_header_footer)
+    new_dict = word_frequency(cleaned_data)
+    t = most_common(new_dict)
+    print('The most common words are:')
+    for freq, word in t[0:20]:
+        print(word, '\t', freq)
+   
+
+if __name__ == '__main__':
+    main()
+        
+
+
 
 import pickle
 
 # Save data to a file (will be part of your data fetching script)
 
-with open('dickens_texts.pickle','w') as f:
-    pickle.dump(data.txt,f)
+# with open('speeches.pickle','w') as f:
+#     pickle.dump(string_list,f)
 
 
-# Load data from a file (will be part of your data processing script)
-with open('dickens_texts.pickle','r') as input_file:
-    reloaded_copy_of_texts = pickle.load(input_file)
+# # Load data from a file (will be part of your data processing script)
+# with open('speeches.pickle','r') as input_file:
+#     reloaded_copy_of_texts = pickle.load(input_file)
+
+
+
+
--- reflection.markdown
+++ reflection.markdown
@@ -2,4 +2,4 @@
 
 ## Project Overview
 
-For my project I decided to use Project Gutenburg and analyze two pieces of writing done by Ernest Hemingway. The goal of looking at Ernest Hemingway specifically was to see the similarities in his writing. To do this I used 
+For my project I decided to use Project Gutenburg and analyze a set of speeched done by Mark Twain. The goal of looking at a variety of speeches specifically was to see the similarities in his diction and what he reffered to most. To do this I first had to plan out my thought process and what I would do if we did not have to code. 
